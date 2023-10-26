ec38fe9bf33e1c939ad64df95227d23285ac8c28
radus7efan <radustef.an@yahoo.ro>
2023-10-26 23:49:00 +0300

    [SMT]: Initial commit

---
+++ src/test/java/com/demo/storemanagementtool/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.storemanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
