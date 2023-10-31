08a7c7ab1aa5cf27beeb9667515334d51e3a87d1
radustefan <rradustefan@gmail.com>
2023-11-01 00:20:23 +0200

    [SMT]: Add test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,14 @@
-INSERT INTO product_entity (id, title, price, quantity, stock_status, discount, discounted_price, description) VALUES (1, 'Brand Keyboard', 254.23, 34, 0, 0, 254.23, 'A short description about the product.');
+INSERT INTO product_entity (id, title, price, quantity, stock_status, discount, discounted_price, description)
+VALUES (1, 'Brand Keyboard', 254.23, 34, 0, 0, 254.23, 'Versatile and ergonomic keyboard.');
+
+INSERT INTO product_entity (id, title, price, quantity, stock_status, discount, discounted_price, description)
+VALUES (2, 'Brand Mouse', 212.00, 20, 0, 0, 212.00, 'A very lightweight and precise mouse.');
+
+INSERT INTO product_entity (id, title, price, quantity, stock_status, discount, discounted_price, description)
+VALUES (3, 'Gaming Headset', 321.00, 4, 2, 0, 321.00, 'High fidelity sound & long lating battery.');
+
+INSERT INTO product_entity (id, title, price, quantity, stock_status, discount, discounted_price, description)
+VALUES (4, 'Phone Charger', 90.00, 0, 1, 25, 67.50, 'Fast 50W charger.');
+
+INSERT INTO product_entity (id, title, price, quantity, stock_status, discount, discounted_price, description)
+VALUES (5, 'Video Camera', 821.99, 5, 0, 15, 698.69, '50mp, 4k@120fps action camera.');
